8defbb1ee3e97acbe974330c39b0affecc83a548
tvio <tvio@centrum.cz>
2025-02-02 22:04:30 +0100

    hotovej pydantic

--- apiconfigload.py
+++ apiconfigload.py
@@ -1,14 +1,10 @@
 from config import logger
 import os
-
-from pydantic import BaseModel, field_validator
+from pydantic import BaseModel, ValidationError
 from pydantic_yaml import parse_yaml_raw_as
 from typing import List,Optional
 
-class ParamTypEnum(str):
-    header = 'header'
-    query = 'query'
-    path = 'path'
+
 class Request(BaseModel):
     class Config:
         extra = 'allow'
@@ -16,28 +12,26 @@ class Response(BaseModel):
     class Config:
         extra = 'allow' 
 class Parameters(BaseModel):
-    typ: ParamTypEnum
+    typ: str
     hodnota: str
-    typ:str
     popis: Optional[str] = None
+    class Config:
+        arbitrary_types_allowed = True
+
 class Jednotlive(BaseModel):
     id: int
-    poradi: int
     metoda: str
     nazev: str
     uri: str
-    parametry: Optional[Parameters] = None
+    parametry: Optional[List[Parameters]] = None
     req: Optional[Request] = None
     res: Optional[Response] = None
 
 class Opakovani(BaseModel):
     pocet: Optional[int] = None
     statusOk: Optional[bool] = None
-    @field_validator('statusOK')
-    def check_true(cls, value):
-        if value is not True:
-            raise ValueError('statusOk objektu Opakovani must be True')
-        return value
+    class Config:
+        arbitrary_types_allowed = True
 class Sekvence(BaseModel):
     id: int
     poradi: int
@@ -46,13 +40,30 @@ class Sekvence(BaseModel):
     uri: str
     req: Optional[Request] = None
     res: Optional[Response] = None
-    parametry: Optional[Parameters] = None
+    parametry: Optional[List[Parameters]] = None
     opakovani: Optional[Opakovani] = None
+    class Config:
+        arbitrary_types_allowed = True
 
+class Interval(BaseModel):
+    hodnota: int
+    jendotka: str
 class Monitor(BaseModel):
     server: str
     odesilatel: str
     emaily: List[str] 
+    pocetVad: int
+    interval: Interval
+    jednotlive: List[Jednotlive]
+
+class Ciselniky(BaseModel):
+    nazev: str
+    metoda: str
+    url : str
+    atribut: str
+    pole: Optional[str] = None
+    class Config:
+        arbitrary_types_allowed = True
 
 class APIConfig(BaseModel):
     id: int
@@ -61,11 +72,12 @@ class APIConfig(BaseModel):
     prostredi : str
     url:str
     jednotlive: List[Jednotlive]
+    #upravit sekvence na objekty zrusit id
     sekvence: List[Sekvence]
-    komponenty: List[Komponenty]
-    monitor: List[Monitor]
-    ciselniky: List[Ciselniky]
-
+    monitor: Monitor
+    ciselniky: List['Ciselniky']
+    class Config:
+        arbitrary_types_allowed = True
 class APIConfigLoader:
     def __init__(self, directory='apiconfigs'):
         self.directory = directory
@@ -73,16 +85,38 @@ class APIConfigLoader:
     def load_config(self, filename):
         filepath = os.path.join(self.directory, filename)
         logger.debug(f"Loading configuration file: {filepath}")
+        if not os.path.exists(filepath):
+            logger.error(f"Configuration file not found: {filepath}")
+            return None
         try:
             with open(filepath, 'r') as file:
                 yaml_data = file.read()
             config = parse_yaml_raw_as(APIConfig, yaml_data)
             logger.debug(f"Configuration loaded successfully: {config}")
             return config
+        except ValidationError as e:
+            logger.error(f"Validation error: {e.errors()}")
+            return None
         except Exception as e:
             logger.error(f"Failed to load configuration file: {e}")
             return None
 
+
 # Example usage:
-# loader = APIConfigLoader()
-# config = loader.load_config('config.yaml')
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Run the application with specified logging level.')
+    parser.add_argument('--log-level', default='INFO', help='Set the logging level (e.g., DEBUG, INFO, WARNING, ERROR, CRITICAL)')
+    args = parser.parse_args()
+
+    # Set the logging level based on the command-line argument
+    logger.setLevel(args.log_level.upper())
+
+    loader = APIConfigLoader()
+    config = loader.load_config('exampleConfig.yaml')
+    if config:
+        #logger.info(f"Loaded configuration: {config}")
+        logger.info(f"monitor: {config.monitor}")
+    else:
+        logger.error("Failed to load configuration.")
--- config.py
+++ config.py
@@ -1,17 +1,32 @@
 import logging
 import logging.config
 import yaml
+import argparse
 
-# Load the logging configuration from the YAML file
-with open('logging_config.yaml', 'r') as file:
-    config = yaml.safe_load(file.read())
+# Load logging configuration from YAML file
+with open('loggingConfig.yaml', 'r') as file:
+    config = yaml.safe_load(file)
     logging.config.dictConfig(config)
 
 # Get the logger specified in the configuration
 logger = logging.getLogger('my_logger')
 
+# Function to set the logging level
+def set_logging_level(level_name):
+    level = getattr(logging, level_name.upper(), None)
+    if not isinstance(level, int):
+        raise ValueError(f'Invalid log level: {level_name}')
+    logger.setLevel(level)
+    for handler in logger.handlers:
+        handler.setLevel(level)
 
-# Example usage
+# Parse command-line arguments
+parser = argparse.ArgumentParser(description='Run the application with specified logging level.')
+parser.add_argument('--log-level', default='INFO', help='Set the logging level (e.g., DEBUG, INFO, WARNING, ERROR, CRITICAL)')
+args = parser.parse_args()
+
+# Set the logging level based on the command-line argument
+set_logging_level(args.log_level)
 # logger.debug('This is a debug message')
 # logger.info('This is an info message')
 # logger.warning('This is a warning message')
